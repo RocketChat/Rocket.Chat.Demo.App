694dc8a700f863e0d5c9dd52f6ec1b9581beb923
Kunal Verma <kunalvrm555@gmail.com>
2022-12-23 02:01:10 +0530

    add comments

--- commands/IncrementCommand.ts
+++ commands/IncrementCommand.ts
@@ -2,7 +2,7 @@ import { IHttp, IModify, IPersistence, IPersistenceRead, IRead } from '@rocket.c
 import { IApp } from '@rocket.chat/apps-engine/definition/IApp';
 import { RocketChatAssociationModel, RocketChatAssociationRecord } from '@rocket.chat/apps-engine/definition/metadata';
 import { ISlashCommand, SlashCommandContext } from '@rocket.chat/apps-engine/definition/slashcommands/index';
-import { sendMessage } from '../lib/sendMessage';
+import { sendMessage } from '../lib/sendMessage';       // this is a helper function to send a message
 export class IncrementCommand implements ISlashCommand {
     app: IApp;
 
@@ -13,7 +13,7 @@ export class IncrementCommand implements ISlashCommand {
     public i18nDescription = 'IncrementCommand_Description';
     public providesPreview = false;
     public i18nParamsExample = '';
-    public associations: Array<RocketChatAssociationRecord> = [
+    public associations: Array<RocketChatAssociationRecord> = [                     // this is the association that will be used to store the value
         new RocketChatAssociationRecord(RocketChatAssociationModel.MISC, 'increment_command'),
     ];
     // debug function
@@ -21,7 +21,7 @@ export class IncrementCommand implements ISlashCommand {
         console.log('test');
         return;
     }
-    public async getValue(read: IRead): Promise<any> {
+    public async getValue(read: IRead): Promise<any> {                     // this function will read the value from the database
         let result: number;
         const persistenceRead = read.getPersistenceReader();
         try {
@@ -55,38 +55,39 @@ export class IncrementCommand implements ISlashCommand {
         persistence: IPersistence,
     ): Promise<void> {
 
-        const sender = context.getSender();
-        const room = context.getRoom();
-        const [subcommand] = context.getArguments();
+        const sender = context.getSender();             // the user who sent the command
+        const room = context.getRoom();                 // the room where the command was sent
+        const [subcommand] = context.getArguments();    // the subcommand
 
         const helpText = `use \`/increment [number]\` to increment the value by a given integer. \n` +
             `use \`/increment [reset|r]\` to reset the value to 0\n` +
             `use \`/increment [help|h]\` to display this message`;
-        if (!subcommand) {
+        if (!subcommand) {                              // no subcommand
             var message = `No Subcommand :thinking: \n ${helpText}`;
             await sendMessage(modify, room, sender, message);
         } else {
             switch (subcommand) {
-                case 'reset':
+                case 'reset':                           // reset subcommand
                 case 'r':
                     await persistence.updateByAssociations(this.associations, { value: 0 }, true);
                     message = `@${sender.username} reset the value to 0.`;
                     await sendMessage(modify, room, sender, message);
                     return;
-                case 'help':
+                case 'help':                            // help subcommand
                 case 'h':
                     await sendMessage(modify, room, sender, helpText);
                     return;
                 default:
-                    if (isNaN(parseInt(subcommand, 10))) {
+                    if (isNaN(parseInt(subcommand, 10))) {  // if the subcommand is not a number
                         message = `@${sender.username} you need to provide a number to increment the value by.`;
                         await sendMessage(modify, room, sender, message);
                         return;
                     } else {
-                        const initialValue = parseInt(await this.getValue(read).then((value) => value.result), 10);
-                        const finalValue = await initialValue + parseInt(subcommand, 10);
-                        await persistence.updateByAssociations(this.associations, { value: finalValue }, true);
-                        message = `@${sender.username} incremented the value of ${initialValue} by ${subcommand}.\nThe new value is ${finalValue} `;
+                        const initialValue = parseInt(await this.getValue(read).then((value) => value.result), 10);     // get the current value
+                        const finalValue = await initialValue + parseInt(subcommand, 10);                               // calculate the new value
+                        await persistence.updateByAssociations(this.associations, { value: finalValue }, true);         // update the value
+                        message = `@${sender.username} incremented the value of ${initialValue} by ${subcommand}.\n` +
+                            `The new value is ${finalValue} `;
                         await sendMessage(modify, room, sender, message);
                     }
             }
